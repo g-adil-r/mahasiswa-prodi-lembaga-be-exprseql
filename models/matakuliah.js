dc7282fe8c4bde7cad68a8c1c1415ae26e29019d
g-adil-r <112382916+g-adil-r@users.noreply.github.com>
2023-11-29 10:41:09 +0700

    Model, migration, seed, new package
    - Menambahkan model mahasiswa, prodi, dan mata kuliah, beserta migration
    - Membuat seed (belum ada isinya)
    - Menambahkan package express-group-route untuk memudahkan group routing

---
+++ models/matakuliah.js
@@ -0,0 +1,23 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Matakuliah extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Matakuliah.init({
+    nama: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Matakuliah',
+  });
+  return Matakuliah;
+};
