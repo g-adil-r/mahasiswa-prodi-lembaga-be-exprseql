2a3f1b0e76479812faf15b8b7cfb723dbc78f2dc
g-adil-r <112382916+g-adil-r@users.noreply.github.com>
2023-11-30 13:31:55 +0700

    Mahasiswa controller and route

--- controller/mahasiswa.controller.js
+++ controller/mahasiswa.controller.js
@@ -1,16 +1,110 @@
-const { Mahasiswa } = require('../models')
+const { 
+    Mahasiswa,
+    Matakuliah
+} = require('../models')
 
 const getAllMahasiswa = async (req, res) => {
     try {
         console.log(Mahasiswa)
         const mahasiswa = await Mahasiswa.findAll();
 
-        res.status(200).json({
+        return res.status(200).json({
             message: "Mendapatkan semua mahasiswa",
             mahasiswa,
         })
     } catch (error) {
-        res.status(500).json({
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const getMahasiswaByToken = async (req, res) => {
+    return res.status(200).json({
+        mahasiswa: req.mahasiswa,
+    });
+}
+
+const getMahasiswaByNim = async (req, res) => {
+    const nim = req.params.nim;
+
+    try {
+        const mahasiswa = await Mahasiswa.findByPk(nim)
+
+        if (!mahasiswa) {
+            return res.status(404).json({
+                message: "Mahasiswa dengan NIM "+nim+" tidak ditemukan",
+            })
+        }
+
+        return res.status(200).json({
+            message: "Mendapatkan mahasiswa dari nim"+nim,
+            mahasiswa,
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const addMataKuliah = async (req, res) => {
+    const nim = req.params.nim;
+    const mkid = req.params.mkid;
+
+    try {
+        const mahasiswa = await Mahasiswa.findByPk(nim)
+        const matakuliah = await Matakuliah.findByPk(nim)
+
+        if (!mahasiswa) {
+            return res.status(404).json({
+                message: "Mahasiswa dengan NIM "+nim+" tidak ditemukan",
+            })
+        }
+        if (!matakuliah) {
+            return res.status(404).json({
+                message: "Mata kuliah dengan ID "+mkid+" tidak ditemukan",
+            })
+        }
+
+        await Mahasiswa.addMataKuliah(matakuliah)
+
+        return res.status(200).json({
+            message: "Menambah matakuliah dari mahasiswa"
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message
+        })
+    }
+}
+
+const removeMataKuliah = async (req, res) => {
+    const nim = req.params.nim;
+    const mkid = req.params.mkid;
+
+    try {
+        const mahasiswa = await Mahasiswa.findByPk(nim)
+        const matakuliah = await Matakuliah.findByPk(nim)
+
+        if (!mahasiswa) {
+            return res.status(404).json({
+                message: "Mahasiswa dengan NIM "+nim+" tidak ditemukan",
+            })
+        }
+        if (!matakuliah) {
+            return res.status(404).json({
+                message: "Mata kuliah dengan ID "+mkid+" tidak ditemukan",
+            })
+        }
+
+        await Mahasiswa.removeMataKuliah(matakuliah)
+
+        return res.status(200).json({
+            message: "Menghapus matakuliah dari mahasiswa"
+        })
+    } catch (error) {
+        return res.status(500).json({
             message: error.message
         })
     }
@@ -18,4 +112,8 @@ const getAllMahasiswa = async (req, res) => {
 
 module.exports = {
     getAllMahasiswa,
+    getMahasiswaByToken,
+    getMahasiswaByNim,
+    addMataKuliah,
+    removeMataKuliah,
 }
--- route/route.js
+++ route/route.js
@@ -1,12 +1,19 @@
 const { default: RouteGroup } = require('express-route-grouping');
 const router = require('express').Router()
 const root = new RouteGroup('/', router)
+
 const mahasiswaController = require("../controller/mahasiswa.controller")
 const prodiController = require("../controller/prodi.controller")
 const matakuliahController = require("../controller/matakuliah.controller")
 
 root.group("/mahasiswa", mahasiswa => {
-    mahasiswa.get("/", mahasiswaController.getAllMahasiswa)
+    mahasiswa.get("/", mahasiswaController.getAllMahasiswa);
+    mahasiswa.get("/:nim", mahasiswaController.getMahasiswaByNim);
+
+    mahasiswa.group("/:nim/matakuliah", matakuliah => {
+        matakuliah.post("/", mahasiswaController.addMataKuliah);
+        matakuliah.put("/", mahasiswaController.removeMataKuliah);
+    })
 })
 
 router.get("/prodi", prodiController.getAllProdi)
